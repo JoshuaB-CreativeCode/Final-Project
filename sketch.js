16a44a37e1a4c69305c28ebaede4902c071a5afd
JoshuaB-CreativeCode <joshuaeb@vt.edu>
2024-12-07 17:17:20 -0500

    No sound now

--- sketch.js
+++ sketch.js
@@ -11,11 +11,6 @@ let circleColor = 0;
 let timeKeeper = 0;
 let failMusic = 0;
 
-//function preload() {
-    //musicSound = loadSound("/Sounds/music.mp3");
-    //pressSound = loadSound("/Sounds/press.mp3");
-    //failSound = loadSound("/Sounds/fail.mp3");
-//}
 
 function setup() {
     //All the setup, including framerate
@@ -57,7 +52,6 @@ function keyReleased() {
 
 function startScreen() {
     //Creates the start screen, which is what the player sees first.
-    //musicSound.loop();
     background(150,150,150);
     textSize(75);
     fill(0);
@@ -195,7 +189,6 @@ function mousePressed() {
     if (mouseX >= circleX - 50 && mouseX <= circleX + 50){
         if (mouseY >= circleY - 50 && mouseY <= circleY + 50) {
             if (circleColor <= 59 && programState == 'game') {
-                //pressSound.play();
                 ridBalls();
                 score++
                 spawner = 1
@@ -209,11 +202,6 @@ function mousePressed() {
 
 // The fail Screen.
 function failScreen() {
-    //musicSound.stop(0);
-    //if (failMusic == 0) {
-        //failSound.play();
-        //failMusic = 1
-    //}
     circleX = -500
     circleY = -500
     background(150,150,150);
